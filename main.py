1a31c393a6732212bfd3c285e3c5451a097d10f5
giridhar30 <giriit30@gmail.com>
2022-04-22 07:39:39 +0530

    added-rest-api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pyfiglet as pfg
 
 print('\n\033[31m' + pfg.figlet_format('E n c R F P', font = 'slant'))
 print('Enhanced non coding RNA Family Prediction tool')
-print('\033[0m\n\t1 - train the model\n\t2 - test accuracy of the model\n\t3 - predict family of ncRNA sequence\n\t0 - exit\n')
+print('\033[0m\n\t1 - train the model\n\t2 - test accuracy of the model\n\t3 - predict family of ncRNA sequence\n\t4 - run as a RESTful service\n\t0 - exit\n')
 option = int(input('enter your option: '))
 print()
 
@@ -39,6 +39,29 @@ def main():
             sys.exit()
         family = predict_family(sequence)
         print('\nThe given RNA Sequence belongs to: \n\033[31m' + family + '\033[0m family\n')
+
+    elif option == 4:
+        # host REST API
+        print('\n\033[31mStarting the application...\033[0m\n')
+
+        from flask import Flask, request
+        from prediction import predict_family
+        app = Flask(__name__)
+
+        @app.route('/', methods = ['POST', 'GET'])
+        def rna():
+            if request.method == 'POST':
+                data = request.json
+                if not data:
+                    return {'error': 'data not found'}
+                if 'rna' not in data:
+                    return {'error': 'RNA sequence not found'}
+                rna = data['rna']
+                if not rna:
+                        return {'error': 'empty sequence not allowed'}
+                return {'family': predict_family(rna)}
+        
+        app.run(debug = True)
 
     else:
         # invalid option
